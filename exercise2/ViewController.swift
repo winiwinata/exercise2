5edd73154542a8b8804762d902698ce14defff58
wini <winiwinata90@gmail.com>
2020-10-29 11:18:30 +0800

    Initial Commit

---
+++ exercise2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exercise2
+//
+//  Created by NDHU_CSIE on 2020/10/29.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
